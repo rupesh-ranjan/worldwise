9f1deb8f3f6ea25d5c4beebbe98e532c81c3ddb0
Rupesh Ranjan <2068799@cognizant.com>
2024-03-04 17:07:23 +0530

    added map, sidebar, city and country list

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,59 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
+
+const URL = "http://localhost:9000";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, seetIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        seetIsLoading(true);
+        const response = await fetch(`${URL}/cities`);
+        const data = await response.json();
+        setCities(data);
+      } catch (error) {
+        alert("There is an error");
+      } finally {
+        seetIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="product" element={<Product />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<Navigate replace to="cities" />} />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="cities/:id" element={<City />} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="form" element={<Form />} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
---
+++ src/components/CityItem.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import styles from "./CityItem.module.css";
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
+
+function CityItem({ city }) {
+  console.log(city);
+  return (
+    <li>
+      <Link
+        className={styles.cityItem}
+        to={`${city.id}?lat=${city.position.lat}&lng=${city.position.lng}`}
+      >
+        <span className={styles.emoji}>{city.emoji}</span>
+        <h3 className={styles.name}>{city.cityName}</h3>
+        <time className={styles.date}>({formatDate(city.date)})</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
+    </li>
+  );
+}
+
+export default CityItem;
