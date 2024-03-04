177ff35e1752049f3c9b81f215bf69aed0fc4060
Rupesh Ranjan <2068799@cognizant.com>
2024-03-04 12:40:27 +0530

    components added

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import styles from "./Sidebar.module.css";
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <p>List of cites</p>
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default Sidebar;
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,9 +1,10 @@
 import AppNav from "../components/AppNav";
+import Sidebar from "../components/Sidebar";
 
 function AppLayout() {
   return (
     <div className="">
-      <AppNav />
+      <Sidebar />
       <h1> App</h1>
     </div>
   );
